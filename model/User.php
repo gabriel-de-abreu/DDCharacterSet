82255f32bfff5d25f72c76bb930bdf0a54bd5eb1
Vinícius Noda <noda.94@gmail.com>
2017-11-30 23:53:52 -0200

    Kapriel chato, bobo e feio

--- model/User.php
+++ model/User.php
@@ -3,7 +3,7 @@
         private $email;
         private $username;
         private $password;
-        
+
         function setEmail($email){
             $this->email=$email;
         }
@@ -14,13 +14,31 @@
             $this->password=$password;
         }
         function getEmail(){
-            return $email;
+            return $this->email;
         }
-        function getUsername($username){
-            return $username;
+        function getUsername(){
+            return $this->username;
         }
         function getPassword(){
-            return $password;
+            return $this->password;
         }
+
+        function createUser(){
+            try{
+                $dbh = new PDO('mysql:host=localhost;dbname=ddtest', "root", "", array(
+                    PDO::ATTR_PERSISTENT => true));
+                $stmp=
+                $connection->prepare("INSERT INTO `ddtest`.`User` (`emailUser`, `senhaUser`, `loginUser`) VALUES (:email,:senha,:login);"); 
+                $stmp->bindParam(":email",$this->email);
+                $stmp->bindParam(":senha",$this->username);
+                $stmp->bindParam(":login",$this->password);
+                $stmp->execute();
+                print_r($this);
+            }catch(PDOException $e){
+                echo "Teste".$e->getMessage();
+                die();
+            }
+        }
+
     }
 ?>
